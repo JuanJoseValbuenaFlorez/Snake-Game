1004740066ce1a6d8f5dbd23641d0448c56398cd
JuanJoseValbuenaFlorez <jflorez24@udi.edu.co>
2025-09-20 17:34:56 -0500

    Add files via upload

--- pygame/main.py
+++ pygame/main.py
@@ -9,22 +9,19 @@ SNAKE_COLOR = (43,52,24)
 cell_size = 30
 numbre_of_cells = 25
 
-offset = 25      
-    
-
 
 class Food:
     def __init__(self, snake_body):
         self.position = self.generate_random_pos(snake_body)
         
     def draw(self):
-        Food_rect = pygame.Rect(offset + self.position.x * cell_size, offset + self.position.y * cell_size,cell_size, cell_size)
+        Food_rect = pygame.Rect(self.position.x * cell_size, self.position.y * cell_size,cell_size, cell_size)
         screen.blit(food_surface, Food_rect)
         
     def generate_random_cell(self):
         x=random.randint(0,numbre_of_cells - 1)
         y  = random.randint(0,numbre_of_cells - 1)
-        return Vector2(x, y)   
+        return Vector2(x, y)
         
     def generate_random_pos(self,snake_body):
         
@@ -43,7 +40,7 @@ class snake:
         
     def draw(self):
         for segment in self.body:
-            segment_rect = pygame.Rect(offset +segment.x * cell_size,offset + segment.y * cell_size, cell_size, cell_size)
+            segment_rect = pygame.Rect(segment.x * cell_size, segment.y * cell_size, cell_size, cell_size)
             pygame.draw.rect(screen, SNAKE_COLOR, segment_rect,0,7)
             
     def update(self):
@@ -52,53 +49,30 @@ class snake:
             self.body.pop()
         else:
             self.add_segment = False
-            
-    def reset(self):
-        self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
-        self.direction = Vector2(1,0)
      
 class Game:
     def __init__(self):
      self.snake = snake()
      self.food = Food(self.snake.body)
-     self.state = "RUNNING"
      
     def draw(self):
          self.snake.draw()
          self.food.draw()
          
     def update(self):
-        if self.state == "RUNNING":
-            self.snake.update()
-            self.check_collision_with_food_()
-            self.check_collision_with_edges()
-            self.check_collision_with_tail()
+        self.snake.update()
+        self.check_collision_with_food_()
         
     def check_collision_with_food_(self):
         if self.snake.body[0] == self.food.position:
             self.food.position = self.food.generate_random_pos(self.snake.body)   
             self.snake.add_segment = True 
-            
-    def check_collision_with_edges (self):
-        if self.snake.body[0].x == numbre_of_cells or self.snake.body[0].x == -1:
-            self.game_over()
-        elif self.snake.body[0].y == numbre_of_cells or self.snake.body[0].y == -1:
-            self.game_over()
-      
-    def game_over(self):
-        self.snake.reset()
-        self.food.position = self.food.generate_random_pos(self.snake.body)
-        self.state = "STOPPED"
-        
-    def check_collision_with_tail(self):
-       headless_snake = self.snake.body[1:]
-       if self.snake.body[0] in headless_snake:
-           self.game_over()
-           
-                 
-screen = pygame.display.set_mode((2*offset + cell_size * numbre_of_cells,  2*offset + cell_size *  numbre_of_cells,))
+    
+         
+       
+screen = pygame.display.set_mode((cell_size * numbre_of_cells, cell_size *  numbre_of_cells,))
 
-food_surface = pygame.image.load("graphics/descarga (1).png")
+food_surface = pygame.image.load("descarga (1).png")
 
 
 pygame.display.set_caption("retro snake")
@@ -122,13 +96,10 @@ while True:
         game.update()
             
       if event.type == pygame.KEYDOWN:
-          if game.state == "STOPPED":
-              game.state = "RUNNING"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-              game.snake.direction = Vector2(1,0)
           if event.key == pygame.K_UP and game.snake.direction != Vector2(0,1):
-              game.snake.direction = Vector2(0,-1)                                                              
+              game.snake.direction = Vector2(0,-1)
           if event.key == pygame.K_DOWN and game.snake.direction != Vector2(0,-1):
-              game.snake.direction = Vector2(0,1)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+              game.snake.direction = Vector2(0,1)
           if event.key == pygame.K_RIGHT and game.snake.direction != Vector2(-1,0):
               game.snake.direction = Vector2(1,0)
           if event.key == pygame.K_LEFT and game.snake.direction != Vector2(1,0):
@@ -137,12 +108,9 @@ while True:
             
             
     screen.fill(BG_COLOR)
-    
-    pygame.draw.rect(screen,SNAKE_COLOR,
-    (offset -5, offset -5, cell_size*numbre_of_cells + 10, cell_size*numbre_of_cells +10)
-    ,5)
-    
+    game.draw()
     game.draw()
     
     pygame.display.update()
     clock.tick(60)
+    
